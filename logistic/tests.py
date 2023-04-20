6a2d950a2ba70f59e241db76584c0e203e7bed92
Alexander Platonov <sasa20sasha19910@yandex.ru>
2023-04-20 10:48:36 +0300

    ADDF

--- logistic/tests.py
+++ logistic/tests.py
@@ -1,9 +1,10 @@
 from unittest import TestCase
 from rest_framework.test import APIClient
 
-class TestSapleView(TestCase):
+
+class TestSampleView(TestCase):
     def test_view_200(self):
         url = '/api/v1/test/'
-        client = APIClient
+        client = APIClient()
         response = client.get(url)
         self.assertEqual(response.status_code, 200)
